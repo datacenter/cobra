5e53fc9f3ca585794bfb4303d0f4718e6c4e9bff
Ravi Chamarthy <chamarty@yahoo.com>
2015-02-24 22:19:00 -0800

    fix(conf.py): Add napoleon extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,6 +53,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
