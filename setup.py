446888baebd10bc8afcb39fb601d00ed6c87cc10
Mike Timm <mtimm@cisco.com>
2015-03-03 13:37:37 -0800

    chore(build): Add python 3.4 to classifiers in setup.py
    Address part of #31

--- setup.py
+++ setup.py
@@ -70,6 +70,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
     ],
     keywords='data center networking configuration management',
     license='http://www.apache.org/licenses/LICENSE-2.0',
